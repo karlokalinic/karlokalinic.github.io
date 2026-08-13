121e73d3677bff2d119b324332524e6bfb7f1eeb
KARLOLEGEND <kalini.karlo@gmail.com>
2026-08-13 02:21:16 +0200

    Persist KARLOLEGEND desktop icon layout

--- karlolegend-desktop/source/Models/DesktopSettings.cs
+++ karlolegend-desktop/source/Models/DesktopSettings.cs
@@ -2,6 +2,16 @@ namespace KarloDiskShell.Models;
 
 public sealed class DesktopSettings
 {
+    public int Schema { get; set; } = 2;
     public string WallpaperPath { get; set; } = "";
     public bool OpenHtmlInsideKarlolegend { get; set; } = true;
+    public bool SnapToGrid { get; set; } = false;
+    public double IconSize { get; set; } = 52;
+    public Dictionary<string, DesktopIconPosition> IconPositions { get; set; } = new();
+}
+
+public sealed class DesktopIconPosition
+{
+    public double X { get; set; }
+    public double Y { get; set; }
 }
