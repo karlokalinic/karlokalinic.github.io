c5e9043964a3610959620fd0bc3d757d7a3a63f3
KARLOLEGEND <kalini.karlo@gmail.com>
2026-08-13 02:21:23 +0200

    Make desktop filesystem items movable

--- karlolegend-desktop/source/Models/FileSystemItem.cs
+++ karlolegend-desktop/source/Models/FileSystemItem.cs
@@ -1,16 +1,52 @@
+using System.ComponentModel;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Windows.Media;
 
 namespace KarloDiskShell.Models;
 
-public sealed class FileSystemItem
+public sealed class FileSystemItem : INotifyPropertyChanged
 {
+    private double _x;
+    private double _y;
+
     public required string Name { get; init; }
     public required string FullPath { get; init; }
     public required bool IsDirectory { get; init; }
     public required ImageSource Icon { get; init; }
 
+    public double X
+    {
+        get => _x;
+        set
+        {
+            if (Math.Abs(_x - value) < 0.1)
+                return;
+
+            _x = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public double Y
+    {
+        get => _y;
+        set
+        {
+            if (Math.Abs(_y - value) < 0.1)
+                return;
+
+            _y = value;
+            OnPropertyChanged();
+        }
+    }
+
     public string Kind => IsDirectory
         ? "Folder"
         : Path.GetExtension(FullPath).TrimStart('.').ToUpperInvariant();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
