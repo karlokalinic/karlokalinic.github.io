87a11c9f55016b9b26afa4abff0add0e673edc96
KARLOLEGEND <kalini.karlo@gmail.com>
2026-08-13 02:13:13 +0200

    Add desktop settings model

---
+++ karlolegend-desktop/source/Models/DesktopSettings.cs
@@ -0,0 +1,7 @@
+namespace KarloDiskShell.Models;
+
+public sealed class DesktopSettings
+{
+    public string WallpaperPath { get; set; } = "";
+    public bool OpenHtmlInsideKarlolegend { get; set; } = true;
+}
