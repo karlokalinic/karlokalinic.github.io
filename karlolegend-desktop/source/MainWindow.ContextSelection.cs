a1f765cd9b12d5ad28987ebfea61f97773df4cbc
KARLOLEGEND <kalini.karlo@gmail.com>
2026-08-15 02:10:18 +0200

    Create item context menu in code instead of XAML style

--- karlolegend-desktop/source/MainWindow.ContextSelection.cs
+++ karlolegend-desktop/source/MainWindow.ContextSelection.cs
@@ -1,4 +1,5 @@
 using KarloDiskShell.Models;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -10,10 +11,50 @@ public partial class MainWindow
         object sender,
         MouseButtonEventArgs e)
     {
-        if (sender is ListBoxItem container &&
-            container.DataContext is FileSystemItem item)
+        if (sender is not ListBoxItem container ||
+            container.DataContext is not FileSystemItem item)
         {
-            ItemsList.SelectedItem = item;
+            return;
         }
+
+        ItemsList.SelectedItem = item;
+
+        // Keep item actions out of the reusable XAML Style. WPF's generated
+        // IComponentConnector can mis-bind event connections for a ContextMenu
+        // nested inside Style/Setter.Value in compiled BAML, which caused the
+        // published EXE to crash during InitializeComponent().
+        var menu = CreateItemContextMenu(container);
+        container.ContextMenu = menu;
+        menu.IsOpen = true;
+
+        // Prevent the ListBox background context menu from opening as well.
+        e.Handled = true;
+    }
+
+    private ContextMenu CreateItemContextMenu(ListBoxItem placementTarget)
+    {
+        var menu = new ContextMenu
+        {
+            PlacementTarget = placementTarget
+        };
+
+        menu.Opened += ItemContextMenu_Opened;
+
+        menu.Items.Add(CreateMenuItem("Open", OpenItemMenuItem_Click));
+        menu.Items.Add(CreateMenuItem("Open Externally", OpenExternalMenuItem_Click));
+        menu.Items.Add(new Separator());
+        menu.Items.Add(CreateMenuItem("Rename", RenameItemMenuItem_Click));
+        menu.Items.Add(CreateMenuItem("Delete to Recycle Bin", DeleteItemMenuItem_Click));
+
+        return menu;
+    }
+
+    private static MenuItem CreateMenuItem(
+        string header,
+        RoutedEventHandler clickHandler)
+    {
+        var item = new MenuItem { Header = header };
+        item.Click += clickHandler;
+        return item;
     }
 }
